57e321743f4eb63b77d9b6e57e613c08664c3745
Nikhil Jain <thinknikhiljain@gmail.com>
2013-04-25 13:24:52 +0530

    Refactored.

---
+++ app/server/controllers/main.js
@@ -0,0 +1,15 @@
+var utils=require('utility');
+var Built=require('built');
+
+exports.create=function(req, res, next){
+    // Eg: console.log('created' );
+}
+exports.update=function(req, res, next){
+    // Eg: console.log('updated' );
+}
+exports.destroy=function(req, res, next){
+    // Eg: console.log('deteted' );
+}
+exports.index=function(req, res, next){
+    // Eg: console.log('home' );
+}
